c685d2f6859ed895ee7ad913e7d6cbdadc12e0c4
dutchakdev <dutchakdev@gmail.com>
2020-01-21 20:02:23 +0200

    get key id from private/public key

--- openpgp/get_key_id.go
+++ openpgp/get_key_id.go
@@ -6,11 +6,11 @@ import (
 	"github.com/keybase/go-crypto/openpgp"
 )
 
-func (o *FastOpenPGP) GetKeyId(privateKey string) (string, error) {
-	rings, err := openpgp.ReadArmoredKeyRing(strings.NewReader(privateKey))
+func (o *FastOpenPGP) GetKeyId(key string) (string, error) {
+	rings, err := openpgp.ReadArmoredKeyRing(strings.NewReader(key))
 	if err != nil {
 		return "", err
 	}
 
-	return rings[0].PrivateKey.KeyIdString(), nil
+	return rings[0].PrimaryKey.KeyIdString(), nil
 }
